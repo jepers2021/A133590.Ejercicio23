34169688950df4ddc0f8d515e2244a2b1462761e
Jorge <jepers2021@gmail.com>
2021-03-29 14:47:51 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace A133590.Ejercicio23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 23");
+            Console.WriteLine("Programa que determina si una fecha ingresada es válida.");
+
+            Console.Write("Por favor, ingrese una fecha: ");
+            DateTime fecha;
+            if(DateTime.TryParse(Console.ReadLine(), out fecha))
+            {
+                Console.WriteLine("La fecha ingresada es válida.");
+            }
+            else
+            {
+                Console.WriteLine("La fecha ingresada no es válida.");
+            }    
+
+            Console.WriteLine("Presione cualquier tecla para continuar..");
+            Console.ReadKey();
+        }
+    }
+}
